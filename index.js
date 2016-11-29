dba437ed2dab4159d978fd2b327e75085231447c
Paschalis Valtsianis <Valitsianius@hotmail.com>
2016-11-29 18:43:33 +0200

    second commit

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
 const express = require('express')
 const bodyParser= require('body-parser')
-const app = express()
-const MongoClient = require('mongodb').MongoClient
+const app = express();
+const MongoClient = require('mongodb').MongoClient;
+var db;
 
-MongoClient.connect('link-to-mongodb', (err, database) => {
-  // ... start the server
-})
+/**MongoClient.connect('mongodb://maendros:maendrosaek4ever@ds113628.mlab.com:13628/random_quotes', (err, database) => {
+ if (err) return console.log(err)
+  db = database
+  app.listen(3000, () => {
+    console.log('listening on 3000')
+  })
+})**/
 
 app.use(bodyParser.urlencoded({extended: true}))
-
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
-  // Note: __dirname is directory that contains the JavaScript source code. Try logging it and see what you get!
-  // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
+  var cursor = db.collection('quotes').find();
 })
 
 app.post('/quotes', (req, res) => {
-  console.log(req.body)
-});
+  db.collection('quotes').save(req.body, (err, result) => {
+    if (err) return console.log(err)
 
-app.listen(3000, function() {
-  console.log('listening  on 3000')
-});
+    console.log('saved to database')
+    res.redirect('/')
+  })
+})
 
-https://zellwk.com/blog/crud-express-mongodb/
-synexisw sto mongolab
+
+/**https://zellwk.com/blog/crud-express-mongodb/
+synexisw sto mongolab**/
